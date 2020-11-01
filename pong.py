3ed5a54a96206f3612fd3471d310ce962346e117
CodeClan Student <codeclanstudent@codeclans-mbp-5.home>
2020-11-01 17:17:32 +0000

    import issue

--- pong.py
+++ pong.py
@@ -1,4 +1,43 @@
-import pygame, sys
+
+import pygame, sys, random 
+
+def ball_animation():
+    global ball_speed_x, ball_speed_y
+    ball.x += ball_speed_x
+    ball.y += ball_speed_y
+
+    if ball.top <= 0 or ball.bottom >= screen_height:
+        ball_speed_y *= -1
+    if ball.left <=0 or ball.right >= screen_width:
+        # ball_speed_x *= -1
+        ball_restart()
+
+    if ball.collidrect(player) or ball.colliderect(opponent):
+        ball_speed_x *= -1
+
+def player_animation():
+    player.y += player_speed
+    if player.top <=0:
+        player.top = 0
+    if player.bottom >= screen_height:
+        player.bottom = screen_height
+
+def opponent_ai():
+    if opponent.top < ball.y:
+        opponent.top += opponent_speed
+    if opponent.bottom > ball.y:
+        opponent.bottom -= opponent_speed
+    if opponent.top <=0:
+        opponent.top = 0
+    if opponent.bottom >= screen_height:
+        opponent.bottom = screen_height
+
+def ball_restart():
+    global ball_speed_x, ball_speed_y
+    ball.center = (screen_width/2, screen_height/2)
+    ball_speed_y *= random.choice((1, -1))
+    ball_speed_x *= random.choice((1, -1))
+
 
 
 # General setup
@@ -28,8 +67,10 @@ bg_color = pygame.Color('grey12')
 light_grey = (200,200,200)
 white = (255, 255,255)
 
-ball_speed_x = 7
-ball_speed_y = 7
+ball_speed_x = 7 * random.choice((1, -1))
+ball_speed_y = 7 * random.choice((1, -1))
+player_speed = 0
+opponent_speed = 7
 
 
 while True:
@@ -38,14 +79,26 @@ while True:
             pygame.quit()
             sys.exit()
 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_DOWN:
+                player_speed +=7
+            if event.key == pygame.K_UP:
+                player_speed -=7
 
-ball.x += ball_speed_y
-ball.y += ball_speed_x
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_DOWN:
+                player_speed -=7
+            if event.key == pygame.K_UP:
+                player_speed +=7
 
-if ball.top <= 0 or ball.bottom >= screen_height:
-    ball_speed_y *= -1
-if ball.left <=0 or ball.right >= screen_width:
-    ball_speed_x *= -1
+
+
+ball_animation()
+player_animation()
+opponent_ai()
+
+    
+
 
 
 # visuals
